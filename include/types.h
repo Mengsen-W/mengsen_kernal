17d48caca222e01d4c8c5aec3da37552b83feacf
Mengsen <mengsen_wang@163.com>
2020-10-21 08:49:59 +0800

    include

---
+++ include/types.h
@@ -0,0 +1,27 @@
+/*
+ * @Author: Mengsen.Wang
+ * @Date: 2020-10-19 19:04:27
+ * @Last Modified by: Mengsen.Wang
+ * @Last Modified time: 2020-10-19 19:06:16
+ */
+
+#ifndef MENGSEN_TYPES_H_
+#define MENGSEN_TYPES_H_
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+#ifndef TRUE
+#define TRUE 1
+#define FALSE 0
+#endif
+
+typedef unsigned int uint32_t;
+typedef int int32_t;
+typedef unsigned short uint16_t;
+typedef short int16_t;
+typedef unsigned char uint8_t;
+typedef char int8_t;
+
+#endif  // INCLUDE_TYPES_H_
---
+++ init/entry.c
@@ -0,0 +1,9 @@
+/*
+ * @Author: Mengsen.Wang
+ * @Date: 2020-10-19 19:02:54
+ * @Last Modified by: Mengsen.Wang
+ * @Last Modified time: 2020-10-19 19:04:07
+ * @Description: entry
+ */
+
+int kern_entry() { return 0; }
